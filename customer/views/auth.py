61e81c6d2f5022689a08a126055f9c549b0b4307
beknazar <beknazarbekazizov@gmail.com>
2024-06-27 12:31:28 +0500

    commit register

---
+++ customer/views/auth.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+
+from customer.forms import LoginForm
+
+
+def login_page(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, email=email, password=password)
+            if user:
+                login(request, user)
+                return redirect('customers')
+    else:
+        form = LoginForm()
+
+    return render(request, 'auth/login.html', {'form': form})
+
+
+def logout_page(request):
+    if request.method == 'GET   ':
+        logout(request)
+        return redirect('customers')
+    return render(request,'auth/logout.html')
+
+def register(request):
+    return render(request, 'auth/register.html')
